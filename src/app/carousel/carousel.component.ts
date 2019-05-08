952cc7b223b7f0855c66f7857a14358d0f628fec
alanxie29 <alanxie29@gmail.com>
2019-05-07 21:26:53 -0400

    homepage

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   imageSources: String[] = [
-    "https://helpx.adobe.com/ca/stock/how-to/visual-reverse-image-search/_jcr_content/main-pars/image.img.jpg/visual-reverse-image-search-v2_1000x560.jpg",
-    "https://www.w3schools.com/w3css/img_lights.jpg",
-    "https://cdn.pixabay.com/photo/2018/02/09/21/46/rose-3142529__340.jpg"  
+    "../assets/angular.png",
+    "../assets/javascript.png",
+    "../assets/mongo.jpg",
+    "../assets/ionic.png",
+    "../assets/react.png",
+    "../assets/react-native.png" 
   ]
   ngOnInit() {
   }
