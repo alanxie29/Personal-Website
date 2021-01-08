dc00859504b1de9bea57b469ed7d74806406e3c6
alanxie29 <alanxie29@gmail.com>
2021-01-07 22:13:04 -0500

    summer 2021 updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './homepage/homepage.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { SlideshowModule } from 'ng-simple-slideshow';
-import { MatChipsModule } from '@angular/material/chips';
-import { FooterComponent } from  './footer/footer.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCardModule,
-    SlideshowModule,
-    MatChipsModule,
-    MatSidenavModule,
+    AppRoutingModule
   ],
 
   providers: [],
